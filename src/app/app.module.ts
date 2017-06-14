21be5091d2d1c33a8d0cc29ad88df652c43110bf
曾经的你 <252352801@qq.com>
2017-06-14 12:13:58 +0800

    删除冗余

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing } from './app.routing';
-import { RootContainerComponent } from '../../components/root-container/root-container.component';
-import { AsideLeftComponent } from '../../components/aside-left/aside-left.component';
-import { HeaderComponent,HeaderLeftComponent,HeaderRightComponent} from '../../components/header/header.component';
-import {NavWrapComponent} from '../../components/nav-wrap/nav-wrap.component';
-import {NavItemComponent} from '../../components/nav-item/nav-item.component';
-import {SubNavItemComponent} from '../../components/sub-nav-item/sub-nav-item.component';
-import {SharedModule} from '../shared/shared.module';
+import { RootContainerComponent } from '../components/root-container/root-container.component';
+import { AsideLeftComponent } from '../components/aside-left/aside-left.component';
+import { HeaderComponent,HeaderLeftComponent,HeaderRightComponent} from '../components/header/header.component';
+import {NavWrapComponent} from '../components/nav-wrap/nav-wrap.component';
+import {NavItemComponent} from '../components/nav-item/nav-item.component';
+import {SubNavItemComponent} from '../components/sub-nav-item/sub-nav-item.component';
+import {SharedModule} from './shared/shared.module';
 
 import { AppComponent } from './app.component';
-import { SigninComponent } from '../signin/signin.component';
-import { IndexComponent } from '../index/index.component';
+import { SigninComponent } from './signin/signin.component';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
