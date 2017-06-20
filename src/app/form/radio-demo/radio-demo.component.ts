d9a994229846cd6d4aee0535d2a00fc18fad99a2
曾经的你 <252352801@qq.com>
2017-06-20 19:09:13 +0800

    package.json

--- src/app/form/radio-demo/radio-demo.component.ts
+++ src/app/form/radio-demo/radio-demo.component.ts
@@ -5,7 +5,7 @@ import { Component,OnInit} from '@angular/core';
     styleUrls: ['./radio-demo.component.less'],
 })
 export class RadioDemoComponent implements OnInit{
-  radio:any=1;
+  radio:any=3;
   constructor(){
   }
 
