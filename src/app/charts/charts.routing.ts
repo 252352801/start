379ac22f2ddc6db57a73b717dcd0b5afb66f79e8
252352801@qq.com <252352801@qq.com>
2018-03-05 16:26:50 +0800

    1.0

--- src/app/charts/charts.routing.ts
+++
@@ -1,9 +0,0 @@
-import { Routes, RouterModule } from '@angular/router';
-import { ChartsComponent }   from './charts.component';
-const routes: Routes = <Routes>[
-  {
-    path: '',
-    component: ChartsComponent
-  }
-];
-export const routing = RouterModule.forChild(routes);
--- src/app/themes/themes.routing.ts
+++
@@ -1,9 +0,0 @@
-import { Routes, RouterModule } from '@angular/router';
-import { ThemesComponent }   from './themes.component';
-const routes: Routes = <Routes>[
-  {
-    path: '',
-    component: ThemesComponent
-  }
-];
-export const routing = RouterModule.forChild(routes);
