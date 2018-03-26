1070a5e7247438d7755e710e8ec55e815beeedb4
252352801@qq.com <252352801@qq.com>
2018-03-26 11:18:51 +0800

    docs/index.html

--- src/app/themes/themes.component.ts
+++ src/app/themes/themes.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnDestroy} from '@angular/core';
 import {SettingService} from '../core/services/setting/setting.service';
 import  {ThemesService}  from '../core/services/themes/themes.service';
+import {ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'themes',
   templateUrl: './themes.component.html',
@@ -10,7 +11,13 @@ export class ThemesComponent implements OnDestroy {
 
   constructor(
     public themesSvc: ThemesService,
+    private actRoute:ActivatedRoute,
     private setting: SettingService) {
+      let skin=this.actRoute.snapshot.params['skin'];
+      console.log(skin);
+      if(skin){
+        this.themesSvc.setSkin(skin);
+      }
   }
 
   ngOnDestroy() {
